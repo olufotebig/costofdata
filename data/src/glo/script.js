a7b04d19da4e792d548eb28bdbad4cb4e66e2ac1
Gbekeloluwa Olufotebi <olufotebigbeke@gmail.com>
2017-11-03 20:05:07 +0100

    add glo data

---
+++ data/src/glo/script.js
@@ -0,0 +1,47 @@
+function main() {
+  let tables = document.getElementsByTagName("table");
+  let data = []
+  for (let i = 0; i < tables.length; i++) {
+    data.push(parseTable(tables[i]));
+  }
+  return {
+    dateNow: Date.now(),
+    data
+  };
+}
+
+function parseTable(table) {
+  let categoryTitle = table.previousElementSibling.innerText;
+
+  let headers = [];
+  let data = [];
+  let tableWidth = table.children[0].children[0].children.length;
+  for (let i = 0; i < tableWidth; i++) {
+    headers.push(table.children[0].children[0].children[i].innerText);
+  }
+
+  for (let i = 1; i < table.children[0].children.length; i++) {
+    let innerData = {};
+    if (table.children[0].children[i].children.length === tableWidth) {
+      for (let j = 0; j < tableWidth; j++) {
+        let value = table.children[0].children[i].children[j].innerText;
+        let akey = headers[j];
+        let obj = {}
+        innerData[akey] = value;
+      }
+      data.push(innerData);
+    }
+
+  }
+
+  return {
+    categoryTitle,
+    data
+  };
+}
+
+let url = 'http://www.gloworld.com/ng/personal/data/data-plans/';
+
+if (window.location.href === url) {
+  copy(main());
+}
